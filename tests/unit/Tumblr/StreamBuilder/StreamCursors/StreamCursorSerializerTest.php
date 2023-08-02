99016e5b8e56fc682e4261e2bd2d82dc155766e3
Alexey Kopytko <alexey@kopytko.com>
2023-08-02 09:19:12 +0900

    Fix it

--- tests/unit/Tumblr/StreamBuilder/StreamCursors/StreamCursorSerializerTest.php
+++ tests/unit/Tumblr/StreamBuilder/StreamCursors/StreamCursorSerializerTest.php
@@ -109,7 +109,7 @@ class StreamCursorSerializerTest extends \PHPUnit\Framework\TestCase
      */
     private function expectExceptionLog()
     {
-        $this->log->expects($this->any())->method('exception');
+        $this->log->expects($this->atLeastOnce())->method('exception');
     }
 
     /**
