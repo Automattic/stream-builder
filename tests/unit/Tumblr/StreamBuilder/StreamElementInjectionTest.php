6a12de8ff262af3ed7f2afd5501047f198ce7d13
lucila <lucilastancato@gmail.com>
2025-10-24 15:14:07 -0300

    remove mutants from StreamElementInjection

--- tests/unit/Tumblr/StreamBuilder/StreamElementInjectionTest.php
+++ tests/unit/Tumblr/StreamBuilder/StreamElementInjectionTest.php
@@ -97,4 +97,206 @@ class StreamElementInjectionTest extends \PHPUnit\Framework\TestCase
         );
         return $stream_ele_injection;
     }
+
+    /**
+     * Test that component is set on element when element has no component
+     * This test verifies that the escaped mutant (removal of setComponent call) would be caught
+     */
+    public function test_component_is_set_when_element_has_no_component()
+    {
+        /** @var StreamInjector|\PHPUnit\Framework\MockObject\MockObject $injector */
+        $injector = $this->getMockBuilder(StreamInjector::class)
+            ->setConstructorArgs(['awesome_injector'])
+            ->getMock();
+        
+        $injector->method('getComponent')
+            ->willReturn('injector_component');
+
+        // Create a test element that tracks component setting
+        $element = new class('test_provider') extends StreamElement {
+            public $component_set_called = false;
+            public $component_value = null;
+            
+            public function setComponent(?string $component): void {
+                $this->component_set_called = true;
+                $this->component_value = $component;
+                parent::setComponent($component);
+            }
+            
+            public function getComponent(): ?string {
+                return $this->component_value;
+            }
+            
+            public function get_element_id(): string {
+                return 'test_element';
+            }
+            
+            public function get_original_element(): StreamElement {
+                return $this;
+            }
+            
+            public function get_parent_element(): StreamElement {
+                return $this;
+            }
+            
+            public function get_cache_key(): string {
+                return 'test_cache_key';
+            }
+            
+            public function add_debug_info(string $header, string $field, $value): void {
+                // No-op
+            }
+            
+            public function get_debug_info(): array {
+                return [];
+            }
+            
+            public function to_string(): string {
+                return 'test_element';
+            }
+            
+            public static function from_template(\Tumblr\StreamBuilder\StreamContext $context) {
+                return new self('test_provider');
+            }
+        };
+
+        new StreamElementInjection($injector, $element);
+        
+        // Verify that setComponent was called
+        $this->assertTrue($element->component_set_called);
+        $this->assertEquals('injector_component', $element->component_value);
+    }
+
+    /**
+     * Test that component is not set when element already has a component
+     */
+    public function test_component_is_not_set_when_element_has_existing_component()
+    {
+        /** @var StreamInjector|\PHPUnit\Framework\MockObject\MockObject $injector */
+        $injector = $this->getMockBuilder(StreamInjector::class)
+            ->setConstructorArgs(['awesome_injector'])
+            ->getMock();
+        
+        $injector->method('getComponent')
+            ->willReturn('injector_component');
+
+        // Create a test element that already has a component
+        $element = new class('test_provider') extends StreamElement {
+            public $component_set_called = false;
+            
+            public function setComponent(?string $component): void {
+                $this->component_set_called = true;
+                parent::setComponent($component);
+            }
+            
+            public function getComponent(): ?string {
+                return 'existing_component';
+            }
+            
+            public function get_element_id(): string {
+                return 'test_element';
+            }
+            
+            public function get_original_element(): StreamElement {
+                return $this;
+            }
+            
+            public function get_parent_element(): StreamElement {
+                return $this;
+            }
+            
+            public function get_cache_key(): string {
+                return 'test_cache_key';
+            }
+            
+            public function add_debug_info(string $header, string $field, $value): void {
+                // No-op
+            }
+            
+            public function get_debug_info(): array {
+                return [];
+            }
+            
+            public function to_string(): string {
+                return 'test_element';
+            }
+            
+            public static function from_template(\Tumblr\StreamBuilder\StreamContext $context) {
+                return new self('test_provider');
+            }
+        };
+
+        new StreamElementInjection($injector, $element);
+        
+        // Verify that setComponent was not called
+        $this->assertFalse($element->component_set_called);
+    }
+
+    /**
+     * Test that component is set when element has empty string component
+     */
+    public function test_component_is_set_when_element_has_empty_component()
+    {
+        /** @var StreamInjector|\PHPUnit\Framework\MockObject\MockObject $injector */
+        $injector = $this->getMockBuilder(StreamInjector::class)
+            ->setConstructorArgs(['awesome_injector'])
+            ->getMock();
+        
+        $injector->method('getComponent')
+            ->willReturn('injector_component');
+
+        // Create a test element with empty component
+        $element = new class('test_provider') extends StreamElement {
+            public $component_set_called = false;
+            public $component_value = null;
+            
+            public function setComponent(?string $component): void {
+                $this->component_set_called = true;
+                $this->component_value = $component;
+                parent::setComponent($component);
+            }
+            
+            public function getComponent(): ?string {
+                return '';
+            }
+            
+            public function get_element_id(): string {
+                return 'test_element';
+            }
+            
+            public function get_original_element(): StreamElement {
+                return $this;
+            }
+            
+            public function get_parent_element(): StreamElement {
+                return $this;
+            }
+            
+            public function get_cache_key(): string {
+                return 'test_cache_key';
+            }
+            
+            public function add_debug_info(string $header, string $field, $value): void {
+                // No-op
+            }
+            
+            public function get_debug_info(): array {
+                return [];
+            }
+            
+            public function to_string(): string {
+                return 'test_element';
+            }
+            
+            public static function from_template(\Tumblr\StreamBuilder\StreamContext $context) {
+                return new self('test_provider');
+            }
+        };
+
+        new StreamElementInjection($injector, $element);
+        
+        // Verify that setComponent was called
+        $this->assertTrue($element->component_set_called);
+        $this->assertEquals('injector_component', $element->component_value);
+    }
 }
